44ebe842a3799e738e046ac14deaf23f535a889e
scenttz <110773126+scenttz@users.noreply.github.com>
2022-11-13 01:04:02 +0700

    test

---
+++ app.py
@@ -0,0 +1,43 @@
+
+import streamlit as st
+import tensorflow as tf
+import cv2
+from PIL import Image, ImageOps
+import numpy as np
+
+@st.cache(allow_output_mutation=True)
+def load_model():
+  model = tf.keras.models.load_model('./model/mobv2_model.h5')
+  return model
+with st.spinner('Model is being loaded..'):
+  model=load_model()
+
+
+st.markdown("<h1 style='text-align: center; color: white;'>Fecal Chicken Disease Diagnostics🐣🐓💩</h1>", unsafe_allow_html=True)
+
+st.title('')
+ 
+file = st.file_uploader("You can check your chicken by fecals's. To detect Unhealthy diseases may be Coccidiosis, Salmonella, Newcastle diseases or Healthy.", type=["jpg", "png", "jpeg"])
+st.set_option('deprecation.showfileUploaderEncoding', False)
+ 
+def upload_predict(upload_image, model):
+        classes = {'Coccidiosis': 0, 'Healthy': 1, 'NCD': 2, 'Salmonella': 3}
+
+        img = tf.keras.preprocessing.image.load_img(
+          upload_image, 
+          target_size=(128, 128))
+        img_array = tf.keras.preprocessing.image.img_to_array(img)
+        img_array = tf.expand_dims(img_array, 0)
+        predictions = model.predict(img_array*1/255.)
+        score = tf.nn.softmax(predictions[0])
+        pred_class=[j for j in classes if classes[j] == np.argmax(score)][0]
+        return pred_class, round(100 * np.max(score),2)
+if file is None:
+    st.text("Please upload an your fecal chicken image file")
+else:
+    image = Image.open(file)
+    st.image(image, use_column_width=True)
+    image_class, score_preds = upload_predict(file, model)
+    st.write("🐥The image is classified as",image_class)
+    st.write("🐔The similarity score is approximately",score_preds)
+    print("The image is classified as ",image_class, "with a similarity score of",score_preds)
